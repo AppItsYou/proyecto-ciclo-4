74394a20bd7c5966d3324bad2b504fcb0f3ab197
hernango <hernanhidelberg@gmail.com>
2021-12-04 17:04:46 -0500

    cambiando rutas a produccion

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,4 @@
 module.exports = {
- db: "mongodb://localhost:27017/perfiles"
- //db:"mongodb+srv://itsyou:Grupo04@cluster0.kzthj.mongodb.net/perfiles?retryWrites=true&w=majority"
+ //db: "mongodb://localhost:27017/perfiles"
+ db:"mongodb+srv://itsyou:Grupo04@cluster0.kzthj.mongodb.net/perfiles?retryWrites=true&w=majority"
 };
